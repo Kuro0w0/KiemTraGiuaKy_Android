267cdc9732cd878bad451bab622c82636083ce7f
Kuro0w0 <minhdungdalat2005@gmail.com>
2025-10-18 09:06:02 +0700

    cau 3 toi 5

--- app/src/main/java/com/example/huynhminhdung_ktgk/ProfileActivity.java
+++ app/src/main/java/com/example/huynhminhdung_ktgk/ProfileActivity.java
@@ -1,6 +1,9 @@
 package com.example.huynhminhdung_ktgk;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,16 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
+
+        Button bt = findViewById(R.id.button2);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (ProfileActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
