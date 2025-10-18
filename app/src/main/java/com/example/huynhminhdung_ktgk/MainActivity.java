f21505a02880517308aef0890ddd056472404139
Kuro0w0 <minhdungdalat2005@gmail.com>
2025-10-18 09:17:16 +0700

    cau 6

--- app/src/main/java/com/example/huynhminhdung_ktgk/MainActivity.java
+++ app/src/main/java/com/example/huynhminhdung_ktgk/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,18 +13,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText name_minhdung_2018;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        Button bt = findViewById(R.id.button);
-        bt.setOnClickListener(new View.OnClickListener() {
+        name_minhdung_2018 = findViewById(R.id.uname);
+        Button minhdung_2108 = findViewById(R.id.button);
+        minhdung_2108.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String dl_minhdung_2108 = name_minhdung_2018.getText().toString();
                 Intent intent = new Intent (MainActivity.this, ProfileActivity.class);
+                intent.putExtra("KEY", dl_minhdung_2108);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/huynhminhdung_ktgk/ProfileActivity.java
+++ app/src/main/java/com/example/huynhminhdung_ktgk/ProfileActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,8 +20,14 @@ public class ProfileActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
 
-        Button bt = findViewById(R.id.button2);
-        bt.setOnClickListener(new View.OnClickListener() {
+        TextView tv_minhdung_2108 = findViewById(R.id.name);
+        Intent hmd_2018 = getIntent();
+        if (hmd_2018 != null && hmd_2018.hasExtra("KEY")){
+            String dl_minhdung_2108 = hmd_2018.getStringExtra("KEY");
+            tv_minhdung_2108.setText(dl_minhdung_2108);
+        }
+        Button minhdung_2108 = findViewById(R.id.button2);
+        minhdung_2108.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent (ProfileActivity.this, MainActivity.class);
